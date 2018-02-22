6377e299c5589b4c17bc3111d06cd4191c8d7953
unknown <fatimah.azzhr@gmail.com>
2018-02-22 15:01:55 +0700

    delete comment

--- seeders/20180222054952-addAddressBulk.js
+++ seeders/20180222054952-addAddressBulk.js
@@ -4,11 +4,9 @@ const fs = require('fs')
 module.exports = {
   up: (queryInterface, Sequelize) => {
     let addresses = fs.readFileSync('./addresses.csv', 'utf8').trim().split('\r\n')
-    // console.log(addresses)
     let arrAddresses = []
     for(let i = 0; i < addresses.length; i++) {
       let address = addresses[i].split(',')
-      // console.log(address)
       let objaddress = {}
       objaddress.street = address[1]
       objaddress.city = address[2]
@@ -18,26 +16,9 @@ module.exports = {
       arrAddresses.push(objaddress)
     }
     return queryInterface.bulkInsert('Addresses', arrAddresses, {});
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Addresses', null, {});
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
   }
 };
