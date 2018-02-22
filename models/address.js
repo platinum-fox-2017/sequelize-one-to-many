5d870bfc634a617ebb0032c6cac5fa85219038a3
haripermadi <harinurpermadi@gmail.com>
2018-02-22 20:12:14 +0700

    edit code untuk list data

--- models/address.js
+++ models/address.js
@@ -8,6 +8,15 @@ module.exports = (sequelize, DataTypes) => {
   });
   address.associate = function(models){
     address.belongsTo(models.contact,{foreignKey: 'id_contact'})
+  };
+  address.prototype.full_address = function (street,city,zip_code) {
+    console.log(`Alamat lengkap : ${street} - ${city} (${zip_code})`)
+  };
+  address.north_area = function(dataAll){
+
+  }
+  address.south_area = function(){
+
   }
 
   return address;
