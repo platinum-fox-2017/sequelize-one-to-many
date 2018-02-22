0245be5344aa58ddc0dace91325bc9b511ba3ae7
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2018-02-22 14:38:09 +0700

    Add contactId on model and migrations

--- migrations/20180222045349-create-address.js
+++ migrations/20180222045349-create-address.js
@@ -17,6 +17,9 @@ module.exports = {
       zipCode: {
         type: Sequelize.STRING
       },
+      contactId: {
+        type: Sequelize.INTEGER
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- models/contact.js
+++ models/contact.js
@@ -3,7 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   var Contact = sequelize.define('Contact', {
     name: DataTypes.STRING,
     email: DataTypes.STRING,
-    phone: DataTypes.STRING
+    phone: DataTypes.STRING,
+    contactId: DataTypes.INTEGER
   }, {});
   Contact.associate = function(models) {
     // associations can be defined here
