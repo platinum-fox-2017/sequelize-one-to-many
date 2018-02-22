1ab3b1913c9be267a0ab2919e3e0dc09858912b3
unknown <fatimah.azzhr@gmail.com>
2018-02-22 15:21:07 +0700

    dipercantik

--- view/index.js
+++ view/index.js
@@ -1,10 +1,25 @@
+const Table = require('cli-table')
+const chalk = require('chalk')
+
 class View {
   static readAll(data) {
-    console.log(data)
+    let table = new Table({
+      head: Object.keys(data[0]),
+    });
+    for(let i = 0; i < data.length; i++) {
+      table.push(Object.values(data[i]))
+    }
+    console.log(chalk.blue.bgWhite(table.toString()))
   }
 
   static findById(data) {
-    console.log(data.dataValues)
+    let table = new Table({
+      head: Object.keys(data.dataValues),
+    });
+    table.push(
+      Object.values(data.dataValues)
+    )
+    console.log(chalk.blue.bgWhite(table.toString()))
   }
 
   static add(obj) {
